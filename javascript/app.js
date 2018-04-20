a70a4420bdd7c777f7e7d3e04570521a3b55b82e
Ryan-Jellesed <ryan.jellesed@umontana.edu>
2018-04-19 23:32:29 -0600

    the multiline chart clear button works

--- javascript/app.js
+++ javascript/app.js
@@ -1,279 +1,155 @@
+var margin = {top: 40, right: 100, bottom: 50, left: 100},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+  
+
+var buildLineMonth = function(ds) {
+
+  // base = 1970;
+  // dataset = ds[year - base].month;
+  // data = dataset.month;
+  // console.log(dataset)
+
+  var minDate = getDate(ds[0]['date']);
+  // console.log(minDate);
+  var maxDate = getDate(ds[ds.length-1].date);
+  // console.log(maxDate);
+
+  console.log("min date: " + minDate);
+  console.log("max date: " + maxDate);
+
+  var tooltip = d3.select("body").append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0)
+
+  // Scales
+  var xScale = d3.time.scale()
+    .domain([minDate, maxDate])
+    .range([0, width]);
+
+  var yScale = d3.scale.linear()
+      .domain([0, d3.max(ds, function(d){ return d.count; })])
+      .range([height, 0])
+      .nice();
+
+  // Axis
+  var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom")
+      .innerTickSize(-height)
+      .outerTickSize(0)
+      .tickPadding(10)
+      .tickFormat(d3.time.format("%b"));
+
+  var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("left")
+      .innerTickSize(-width)
+      .outerTickSize(0)
+      .tickPadding(10);
+
+  // Line func
+  var line = d3.svg.line()
+      .x(function(d) { return xScale(getDate(d.date)); })
+      .y(function(d) { return yScale(d.count); })
+      .interpolate("linear");
+
+  // Create svg
+  var svg = d3.select("#area-1").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("id", "svg-month")
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis)
+
+    // text label for the x axis
+    // svg.append("text")             
+    //       .attr("transform",
+    //             "translate(" + (width/2) + " ," + 
+    //                            (height + margin.top + 10) + ")")
+    //       .style("text-anchor", "middle")
+    //       .text("Count of Global Terror Attacks")
+    //       .text("1970 - 2016");
 
 
+    svg.append("path")
+        .data([ds])
+        .attr("class", "path-month")
+        .attr("d", line);
+
+    var dots = svg.selectAll("circle")
+                    .data(ds)
+                    .enter()
+                    .append("circle")
+                    .attr({
+                      cx: function(d) { return xScale(getDate(d.date)); },
+                      cy: function(d) { return yScale(d.count); },
+                      r: 3,
+                      "fill": "#666666",
+                      class: "circle-svg"
+                    })
+                    .on("mouseover", function(d){
+
+                      tooltip.transition()
+                                .duration(500)
+                                .style("opacity", .85)
+                      tooltip.html("<strong>Attack Count: " + d.count)
+                                .style("left", (d3.event.pageX) + "px")
+                                .style("top", (d3.event.pageY - 28) + "px");
+                    })
+                    .on("mouseout", function(d){
+                      tooltip.transition()
+                                .duration(300)
+                                .style("opacity", 0);
+                    })
 
 
-  var margin = {top: 40, right: 100, bottom: 50, left: 100},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    svg.append("text")
+          .attr("x", (width /2 ))
+          .attr("y", 0 - (margin.top / 5))
+          .attr("text-anchor", "middle")
+          .style("font-size", "16px")
+          .style("text-decoration", "underline")
+          .text("Count of Global Terror Attacks 1970 - 2016 ")
+    svg.append("text")
+          .attr("x", (width - 60 ))
+          .attr("y", 0 - (margin.top /5))
+          .attr("text-anchor", "right")
+          .style("font-size", "30px")
+          .style("text-decoration", "bold")
+          .style("fill", "black")
+          .text("")
+          .attr("id", "demo")
+    svg.append("text")
+          .attr("x", (width - 150 ))
+          .attr("y", 0 - (margin.top /5))
+          .attr("text-anchor", "right")
+          .style("font-size", "30px")
+          .style("text-decoration", "bold")
+          // .text("1970")
+          .style("fill", "red")
+          .attr("id", "demo2")
+    // svg.append("text")
+    //       .attr("x", (width - 70 ))
+    //       .attr("y", 0 - (margin.top /5))
+    //       .attr("text-anchor", "right")
+    //       .style("font-size", "30px")
+    //       .style("text-decoration", "bold")
+    //       .text("U.S.")
+    //       .attr("id", "gdp-year")
+          // .text("1970 - 2016");
 
-  /*
-    Start building the graphing functions
-   */
-
- 
-  // var buildLine = function(ds){
-
-  //   var minDate = getDate(ds[0].date);
-
-  //   var maxDate = getDate(ds[ds.length-1].date);
-
-  //   console.log("min date: " + minDate);
-  //   console.log("max date: " + maxDate);
-
-  //   var tooltip = d3.select('body').append("div")
-  //                     .attr("class", "tooltip")
-  //                     .style("opacity", 0)
-
-  //   // scales
-  //   var xScale = d3.time.scale()
-  //                         .domain([minDate, maxDate])
-  //                         .range([0, width]);
-
-  //   var yScale = d3.scale.linear()
-  //                           .domain([0, d3.max(ds. function(d){ return d.count; })])
-  //                           .range([height, 0])
-  //                           .nice();
-
-  //   // Axis
-  //   var xAxis = d3.svg.axis()
-  //                       .scale(xScale)
-  //                       .orient("bottom")
-  //                       .innerTickSize(-height)
-  //                       .outerTickSize(0)
-  //                       .tickPadding(10)
-  //                       .tickFormat(d3.time.format("%b"));
-
-  //   var yAxis = d3.svg.axis()
-  //       .scale(yScale)
-  //       .orient("left")
-  //       .innerTickSize(-width)
-  //       .outerTickSize(0)
-  //       .tickPadding(10);
-
-
-  // }
-
-
-  var buildLineYear = function(data) {
-
-    dataset = data;
-    console.log(dataset)
-
-    var minDate = getDate(dataset[0].date );
-
-    var maxDate = getDate(dataset[dataset.length -1].date );
-
-    console.log("min date: " + minDate);
-    console.log("max date: " + maxDate);
-
-    // Scales
-    var xScale = d3.time.scale()
-      .domain([minDate, maxDate])
-      .range([0, width]);
-
-    var yScale = d3.scale.linear()
-        .domain([0, d3.max(dataset, function(d){ return d.count; })])
-        .range([height, 0]);
-
-    // Axis
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom")
-        .innerTickSize(-height)
-        .outerTickSize(0)
-        .tickPadding(10)
-        .tickFormat(d3.time.format("%y'"));
-
-    var yAxis = d3.svg.axis()
-        .scale(yScale)
-        .orient("left")
-        .innerTickSize(-width)
-        .outerTickSize(0)
-        .tickPadding(10);
-
-    // Line func
-    var line = d3.svg.line()
-        .x(function(d) { return xScale(getDate(d.date ))})
-        .y(function(d) { return yScale(d.count); })
-        .interpolate("linear");
-
-    // Create svg
-    var svg = d3.select("").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .attr("id", "svg-month")
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      svg.append("g")
-          .attr("class", "x-axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis)
-
-      svg.append("g")
-          .attr("class", "y-axis")
-          .call(yAxis)
-
-      // text label for the x axis
-      svg.append("text")             
-      .attr("transform",
-            "translate(" + (width/2) + " ," + 
-                           (height + margin.top + 10) + ")")
-      .style("text-anchor", "middle");
-      // .text("Month Number or Year Number (I haven't coded the dynamic variables yet)");
-
-      svg.append("path")
-          .data([dataset])
-          .attr("class", "path-month")
-          .attr("d", line);
-  };
-
-  var buildLineMonth = function(ds) {
-
-    // base = 1970;
-    // dataset = ds[year - base].month;
-    // data = dataset.month;
-    // console.log(dataset)
-
-    var minDate = getDate(ds[0]['date']);
-    // console.log(minDate);
-    var maxDate = getDate(ds[ds.length-1].date);
-    // console.log(maxDate);
-
-    console.log("min date: " + minDate);
-    console.log("max date: " + maxDate);
-
-    var tooltip = d3.select("body").append("div")
-                      .attr("class", "tooltip")
-                      .style("opacity", 0)
-
-    // Scales
-    var xScale = d3.time.scale()
-      .domain([minDate, maxDate])
-      .range([0, width]);
-
-    var yScale = d3.scale.linear()
-        .domain([0, d3.max(ds, function(d){ return d.count; })])
-        .range([height, 0])
-        .nice();
-
-    // Axis
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom")
-        .innerTickSize(-height)
-        .outerTickSize(0)
-        .tickPadding(10)
-        .tickFormat(d3.time.format("%b"));
-
-    var yAxis = d3.svg.axis()
-        .scale(yScale)
-        .orient("left")
-        .innerTickSize(-width)
-        .outerTickSize(0)
-        .tickPadding(10);
-
-    // Line func
-    var line = d3.svg.line()
-        .x(function(d) { return xScale(getDate(d.date)); })
-        .y(function(d) { return yScale(d.count); })
-        .interpolate("linear");
-
-    // Create svg
-    var svg = d3.select("#area-1").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .attr("id", "svg-month")
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      svg.append("g")
-          .attr("class", "x-axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis)
-
-      svg.append("g")
-          .attr("class", "y-axis")
-          .call(yAxis)
-
-      // text label for the x axis
-      // svg.append("text")             
-      //       .attr("transform",
-      //             "translate(" + (width/2) + " ," + 
-      //                            (height + margin.top + 10) + ")")
-      //       .style("text-anchor", "middle")
-      //       .text("Count of Global Terror Attacks")
-      //       .text("1970 - 2016");
-
-
-      svg.append("path")
-          .data([ds])
-          .attr("class", "path-month")
-          .attr("d", line);
-
-      var dots = svg.selectAll("circle")
-                      .data(ds)
-                      .enter()
-                      .append("circle")
-                      .attr({
-                        cx: function(d) { return xScale(getDate(d.date)); },
-                        cy: function(d) { return yScale(d.count); },
-                        r: 3,
-                        "fill": "#666666",
-                        class: "circle-svg"
-                      })
-                      .on("mouseover", function(d){
-
-                        tooltip.transition()
-                                  .duration(500)
-                                  .style("opacity", .85)
-                        tooltip.html("<strong>Attack Count: " + d.count)
-                                  .style("left", (d3.event.pageX) + "px")
-                                  .style("top", (d3.event.pageY - 28) + "px");
-                      })
-                      .on("mouseout", function(d){
-                        tooltip.transition()
-                                  .duration(300)
-                                  .style("opacity", 0);
-                      })
-
-
-      svg.append("text")
-            .attr("x", (width /2 ))
-            .attr("y", 0 - (margin.top / 5))
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .style("text-decoration", "underline")
-            .text("Count of Global Terror Attacks 1970 - 2016 ")
-      svg.append("text")
-            .attr("x", (width - 60 ))
-            .attr("y", 0 - (margin.top /5))
-            .attr("text-anchor", "right")
-            .style("font-size", "30px")
-            .style("text-decoration", "bold")
-            .style("fill", "black")
-            .text("")
-            .attr("id", "demo")
-      svg.append("text")
-            .attr("x", (width - 150 ))
-            .attr("y", 0 - (margin.top /5))
-            .attr("text-anchor", "right")
-            .style("font-size", "30px")
-            .style("text-decoration", "bold")
-            // .text("1970")
-            .style("fill", "red")
-            .attr("id", "demo2")
-      // svg.append("text")
-      //       .attr("x", (width - 70 ))
-      //       .attr("y", 0 - (margin.top /5))
-      //       .attr("text-anchor", "right")
-      //       .style("font-size", "30px")
-      //       .style("text-decoration", "bold")
-      //       .text("U.S.")
-      //       .attr("id", "gdp-year")
-            // .text("1970 - 2016");
-
-  };
+};
 
   var updateLineMonth = function(ds) {
 
@@ -366,60 +242,31 @@
                       });                     
   };
 
-// var obj = new Object();
-//    obj.name = "Raj";
-//    obj.age  = 32;
-//    obj.married = false;
-//    var jsonString= JSON.stringify(obj);
 
-// var jsonString = JSON.stringify(ds2);
+var gtdMini = [];
+d3.csv("large_data/gtdMini.csv", function(error, data) {
 
-// JSON.parse(jsonString);
-// decodedData = JSON.parse(jsonString);
-
-// decodedData.contents.forEach(function(ds){
-//   console.log(ds);
-// })
-
-
-
+  if(error){
+    console.log(error);
+  } else {
+    console.log("********** gtdMini Loaded Successfully ***************");
   
-
-
-
-  /*
-    Load the whole gtd dataset in JSON format
-   */
-  var gtdJSON = [];
-  d3.json("large_data/gtd.json", function(error, data){
-
-    if(error){
-      console.log(error);
-    } else {
-      // console.log("************************************************");
-      // console.log("Loaded the GTD full Dataset!!!");
-      // console.log(data.contents);
-      // gtdJSON = data;
-      // console.log("************************************************\n\n\n\n");
+    data.forEach(function(ds){
+      gtdMini.push(ds);
     
-      console.log("*******    gtdJSON = GTD full dataset      *******");
-      data.contents.forEach(function(contents){
-        gtdJSON.push(contents);
-      });
-      console.log(gtdJSON);
-      console.log("**************************************************\n\n\n\n");
+    });
 
-    }
+    console.log(gtdMini);
+    console.log("*******************************************************");
+  }
 
-  });
+});  
 
 
-  /*
-    when pulling data from github use the api
-   */
 
   // d3.json("https://api.github.com/repos/Ryan-Jellesed/gtd/contents/jsonFiles/MM_YY_Atacks.json", function(error, data) {
-  d3.json("jsonFiles/MM_YY_Atacks.json", function(error, data) {
+  // d3.json("jsonFiles/MM_YY_Atacks.json", function(error, data) {
+  d3.csv("large_data/")
 
    if(error) {
        console.log(error);
@@ -660,6 +507,21 @@
 });
 
 
+d3.json("http://api.worldbank.org/v2/countries/eg/indicators/NY.GDP.MKTP.CD?date=1970&format=json", function(error, data) {
+     
+    if(error) {
+       console.log(error);
+    } else {
+       // console.log(data); //we're golden! 
+    }
+
+      // var decodedData = JSON.parse(window.atob(data.content));
+      countryLiteracyRate = data;
+      return(countryLiteracyRate);
+  });
+
+
+
 /*
   function for saving JSON file
   console.save(data, filename);
@@ -727,6 +589,7 @@ var compareYears = function () {
 
   }
 };
+
 
 
 // gets the value that is in the rendered html not the value="" from within the tag
@@ -977,30 +840,30 @@ var selectRegion = function(sel){
 };
 
 // this code block will set up an array of lat lon value to be used in plotting on the map
-// it iterates over the gtdJSON file and pushes the lat lon data to the new array 
+// it iterates over the gtdMini file and pushes the lat lon data to the new array 
 // latLonArray = [];
 var regionLatLon = function(){
   var latLonArray = [];
   var selectedYear = document.getElementById("year-option").value;
   // var selectedRegion = document.getElementById("regions_all").value;
-  for(i = 0; i < gtdJSON.length; i++){
+  for(i = 0; i < gtdMini.length; i++){
 
-    if(gtdJSON[i]['iyear'] === selectedYear & gtdJSON[i]['latitude'] !== "" & gtdJSON[i]['region_txt'] === regionSelected) {
+    if(gtdMini[i]['iyear'] === selectedYear & gtdMini[i]['latitude'] !== "" & gtdMini[i]['region_txt'] === regionSelected) {
       latLonArray.push({
-                      lon: parseFloat(gtdJSON[i]['longitude']),
-                      lat: parseFloat(gtdJSON[i]['latitude']),
-                      date: String(gtdJSON[i]['imonth']) + "/" + String(gtdJSON[i]['iday']) + "/" + String(gtdJSON[i]['iyear']),
-                      country: gtdJSON[i]['country_txt'],
-                      city: gtdJSON[i]['city'],
-                      state: gtdJSON[i]['provstate'],
-                      target: gtdJSON[i]['target1'],
-                      group_name: gtdJSON[i]['gname'],
-                      motive: gtdJSON[i]['motive'],
-                      attack_type: gtdJSON[i]['attacktype1_txt'],
-                      weapon_type: gtdJSON[i]['weaptype1_txt'],
-                      number_killed: gtdJSON[i]['nkill'],
-                      summary: gtdJSON[i]['summary'],
-                      additional_notes: gtdJSON[i]['addnotes']
+                      lon: parseFloat(gtdMini[i]['longitude']),
+                      lat: parseFloat(gtdMini[i]['latitude']),
+                      date: String(gtdMini[i]['imonth']) + "/" + String(gtdMini[i]['iday']) + "/" + String(gtdMini[i]['iyear']),
+                      country: gtdMini[i]['country_txt'],
+                      city: gtdMini[i]['city'],
+                      state: gtdMini[i]['provstate'],
+                      target: gtdMini[i]['target1'],
+                      group_name: gtdMini[i]['gname'],
+                      motive: gtdMini[i]['motive'],
+                      attack_type: gtdMini[i]['attacktype1_txt'],
+                      weapon_type: gtdMini[i]['weaptype1_txt'],
+                      number_killed: gtdMini[i]['nkill'],
+                      summary: gtdMini[i]['summary'],
+                      additional_notes: gtdMini[i]['addnotes']
                 });
     }
   };
